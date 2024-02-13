a07630dac775fa3fa918b9faad64c2379fabcbc6
ez-github-dev <142704916+ez-github-dev@users.noreply.github.com>
2024-02-13 22:14:43 +0800

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -10,6 +10,6 @@ terraform {
 provider "aws" {
 #   shared_config_files      = ["/home/vagrant/.aws/config"]
 #   shared_credentials_files = ["/home/vagrant/.aws/credentials"]
-   profile                  = "ezc-master"
+#   profile                  = "ezc-master"
   region                   = var.aws_region
 }
